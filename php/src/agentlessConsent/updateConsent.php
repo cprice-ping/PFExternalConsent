44522025cd456d94b34b097097b7b3a62db3caa0
Chris Price <cprice@pingidentity.com>
2019-08-04 09:04:38 -0700

    Fixing the code swap

--- php/src/agentlessConsent/updateConsent.php
+++ php/src/agentlessConsent/updateConsent.php
@@ -185,7 +185,6 @@ $data2_string = json_encode($data2);
 
 $curl = curl_init();
 
-
 curl_setopt_array($curl, array(
   CURLOPT_PORT => $pingfedPort,
   CURLOPT_URL => $pingfed. "/ext/ref/dropoff",
@@ -208,7 +207,6 @@ curl_setopt_array($curl, array(
   ),
 ));
 
-
 $response = curl_exec($curl);
 $err = curl_error($curl);
 
@@ -223,102 +221,10 @@ $responseData = json_decode($response, false);
 
 $newREF = $responseData->REF;
 
-
-
 // REDIRECT USER BACK TO PF RESUME URL
 $finalURL = $pingfed. ":" . $pingfedPort . $resumePath . "?REF=" . $newREF;
 
 header("Location: $finalURL"); 
 exit();
 
-
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>OAuth External Consent Demo</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-  <!-- Custom styles for this template -->
-  <link href="css/clean-blog.min.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand" href="index.html"></a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="index.html"></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.html"></a>
-          </li>
-         
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="site-heading">
-            <h1>External Consent App Approval</h1><br/>
-           
-           </div>
-        </div>
-      </div>
-    </div>
-  </header>
-
-  
-  <!-- Footer -->
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          
-          <p class="copyright text-muted">Copyright &copy; Remy&Chris 2019</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
-
-</body>
-
-</html>
